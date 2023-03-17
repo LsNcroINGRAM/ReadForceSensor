9573007f89bc4abb7c641d4b0c039eeecac998b9
Ingram <ingram0192@gmail.com>
2023-03-17 19:22:14 +0800

    2023/3/17 19:22 final revise

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #define TEST 0
+#define VCM_MODELING 1
 
 #include <stdio.h>
 #include "Wacoh.h"
@@ -11,7 +12,7 @@ int main()
 	serial_connect("\\\\.\\COM10"); //connect to the comport, you must add \\\\.\\ in front of the name.
 	float data[6] = { 0 }; //the container to save the six-dimension data from the force sensor
 
-	#if !TEST
+	#if !TEST && !VCM_MODELING
 		FILE* fpt; //file pointer
 		unsigned int x_value = 0;
 		const int size = 100;
@@ -26,12 +27,12 @@ int main()
 				y[i] = y[i + 1];
 			}
 			WacohRead(data); //get the data
-			if (data[2] > 10) //discard the large error
+			if (data[2] > 100) //discard the large error
 			{
-				continue;
+				break;
 			}
 			x[size - 1] = x_value++; //fill the last entry in x
-			y[size - 1] = - (data[2] - 9.4); //fill the last entry in y
+			y[size - 1] = 4.7 - data[2]; //fill the last entry in y
 
 			if (x_value > (size * 100)) //make sure x_vaule won't overflow
 			{
@@ -49,11 +50,11 @@ int main()
 
 			fclose(fpt);
 
-			Sleep(100);
+			Sleep(20);
 }
 	#endif
 
-	#if TEST
+	#if TEST && !VCM_MODELING
 		float sum[6] = { 0 };
 		int count = 0;
 		int times = 500;
@@ -68,6 +69,24 @@ int main()
 		}
 		printf("Fx:%.2f	Fy:%.2f	Fz:%.2f	Mx:%.2f	My:%.2f	Mz:%.2f\n",
 			sum[0] / times, sum[1] / times, sum[2] / times, sum[3] / times, sum[4] / times, sum[5] / times);
+	#endif
+
+	#if VCM_MODELING
+		int count = 0;
+		double total = 0;
+		double ans = 0;
+		for (int i = 0; i < 250; i++)
+		{
+			WacohRead(data);
+			if (data[2] > 50) //discard the large error
+			{
+				continue;
+			}
+			total = total + 4.79 - data[2];
+			count++;
+		}
+		ans = total / count;
+		printf("%.2f", ans);
 	#endif
 
 	return 0;
